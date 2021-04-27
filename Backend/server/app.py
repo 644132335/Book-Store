64b7b45b64e758932cece806ae02a1430db66334
Kaiyang Jiang <Kaiyang Jiang>
2021-04-27 10:52:41 +0800

    implement date

--- Backend/server/app.py
+++ Backend/server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookstore.db'
@@ -18,9 +19,19 @@ class Manager(db.Model):
     ManagerId = db.Column(db.String(20),nullable=False, primary_key=True)
     ManagerName = db.Column(db.String(20))
 
+#book table
+class Book(db.Model):
+    ISBN = db.Column(db.Integer,nullable=False,primary_key=True)
+    title = db.Column(db.String(30),nullable=False)
+    page = db.Column(db.Integer)
+    date = db.Column(db.DateTime,nullable=False, default=datetime.utcnow)
+    publisher = db.Column(db.String(30), nullable=False)
+    language = db.Column(db.String(30),nullable=False,default="English")
+    stock = db.Column(db.Integer,nullable=False)
+    price = db.Column(db.Float,nullable=False)
 
 def managerInit():
-    superMana=Manager(ManagerId="644132")
+    superMana=Manager(ManagerId="644132",ManagerName="Matt")
     db.session.add(superMana)
     db.session.commit()
 
@@ -111,22 +122,64 @@ def signup():
         users=User.query.all()
         return render_template('signup.html',tasks=users)
 
-# manager login
+#manager login
 @app.route('/managerLogin',methods=['POST','GET'])
 def managerlogin():
     managers=Manager.query.all()
     if request.method == 'POST':
-        managerID=request.form['managerID']
+        managerID=request.form['managerId']
 
         for manager in managers:
-            if(manager.ManagerID==managerID):
-                return "Manager Login Success"
+            if(manager.ManagerId==managerID):
+                return redirect('/managerAdd')
         return "The manager ID is wrong"
     else:      
         return render_template('managerLogin.html',allmana=managers)
 
+#manager add
+@app.route('/managerAdd',methods=['POST','GET'])
+def managerAdd():
+    if request.method == 'POST':
+        managerID=request.form['managerId']
+        managerName=request.form['managerName']
+        if(managerID!='' and managerName!=''):
+            newmana=Manager(ManagerId=managerID, ManagerName=managerName)
+        else:
+            return "manager ID or name cannot be empty"
+       
+        try:
+            db.session.add(newmana)
+            db.session.commit()
+            return redirect('/managerAdd')
+        except:
+            return "something wrong when adding new manager"
+    else:
+        managerss=Manager.query.all()
+        return render_template('managerControl.html',managers=managerss)
+
+#book add
+@app.route('/bookAdd',methods=['POST','GET'])
+def bookAdd():
+    books=Book.query.all()
+    if request.method == 'POST':
+        title=request.form['title']
+        ISBN=request.form['ISBN']
+        page=request.form['page']
+        date=request.form['date']
+        publisher=request.form['publisher']
+        language=request.form['language']
+        stock=request.form['stock']
+        price=request.form['price']
+        newbook=Book(ISBN=ISBN,title=title,page=page,date=date,publisher=publisher,language=language
+        ,stock=stock,price=price)
+        
+        db.session.add(newbook)
+        db.session.commit()
+
+    else:
+        return render_template('bookAdd.html',allBook=books)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000,debug=True)
